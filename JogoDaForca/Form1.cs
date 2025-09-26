3ffcf319e5ace31885b7cd47092d8e8a53dccf54
vinicius-goess <viny_lolo@hotmail.com>
2025-09-26 16:54:43 -0300

    Melhora do boneco!!!

--- JogoDaForca/Form1.cs
+++ JogoDaForca/Form1.cs
@@ -87,21 +87,70 @@ namespace JogoDaForca
             Bitmap bmp = new Bitmap(pictureBoxForca.Width, pictureBoxForca.Height);
             using (Graphics g = Graphics.FromImage(bmp))
             using (Pen pen = new Pen(Color.DarkBlue, 3))
+            using (Brush bodyBrush = new SolidBrush(Color.DarkBlue))
+            using (Brush headBrush = new SolidBrush(Color.Beige))  // Cabeça cor de pele
+            using (Brush eyeBrush = new SolidBrush(Color.Black))   // Olhos pretos
             {
                 g.Clear(this.BackColor);
+
                 // Estrutura da forca
                 g.DrawLine(pen, 20, 250, 150, 250); // Base
                 g.DrawLine(pen, 85, 250, 85, 50);   // Poste
                 g.DrawLine(pen, 85, 50, 200, 50);   // Topo
                 g.DrawLine(pen, 200, 50, 200, 80);  // Corda
 
-                if (erros >= 1) g.DrawEllipse(pen, 175, 80, 50, 50);    // Cabeça
-                if (erros >= 2) g.DrawLine(pen, 200, 130, 200, 200);   // Tronco
-                if (erros >= 3) g.DrawLine(pen, 200, 150, 170, 180);   // Braço esquerdo
-                if (erros >= 4) g.DrawLine(pen, 200, 150, 230, 180);   // Braço direito
-                if (erros >= 5) g.DrawLine(pen, 200, 200, 170, 240);   // Perna esquerda
-                if (erros >= 6) g.DrawLine(pen, 200, 200, 230, 240);   // Perna direita
+                // Cabeça
+                if (erros >= 1)
+                {
+                    g.FillEllipse(headBrush, 170, 80, 60, 60); // Cabeça
+                    g.DrawEllipse(pen, 170, 80, 60, 60);       // Contorno da cabeça
+
+                    // Olhos
+                    g.FillEllipse(eyeBrush, 185, 100, 6, 6);
+                    g.FillEllipse(eyeBrush, 205, 100, 6, 6);
+
+                    // Boca triste
+                    g.DrawArc(new Pen(Color.Black, 2), 185, 115, 30, 15, 200, 140);
+
+                    // Cabelo (risquinhos no topo da cabeça)
+                    g.DrawLine(pen, 185, 75, 185, 85);
+                    g.DrawLine(pen, 200, 72, 200, 85);
+                    g.DrawLine(pen, 215, 75, 215, 85);
+                }
+
+                // Tronco
+                if (erros >= 2)
+                    g.FillRectangle(bodyBrush, 185, 140, 30, 70);
+
+                // Braço esquerdo
+                if (erros >= 3)
+                {
+                    g.DrawLine(new Pen(Color.DarkBlue, 8), 185, 150, 150, 190);
+                    g.FillEllipse(bodyBrush, 142, 185, 10, 10); // mão
+                }
+
+                // Braço direito
+                if (erros >= 4)
+                {
+                    g.DrawLine(new Pen(Color.DarkBlue, 8), 215, 150, 250, 190);
+                    g.FillEllipse(bodyBrush, 248, 185, 10, 10); // mão
+                }
+
+                // Perna esquerda
+                if (erros >= 5)
+                {
+                    g.DrawLine(new Pen(Color.DarkBlue, 10), 190, 210, 170, 250);
+                    g.FillEllipse(bodyBrush, 162, 245, 15, 8); // pé
+                }
+
+                // Perna direita
+                if (erros >= 6)
+                {
+                    g.DrawLine(new Pen(Color.DarkBlue, 10), 210, 210, 230, 250);
+                    g.FillEllipse(bodyBrush, 222, 245, 15, 8); // pé
+                }
             }
+
             pictureBoxForca.Image = bmp;
         }
 
